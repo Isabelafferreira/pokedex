80d68b2713459795fd002f3d5901c1c670189c51
isabelafferreira <isabela.ff00@gmail.com>
2023-12-04 21:53:21 -0300

    feat: added modal styles with bg black

--- src/Pokedex/Pokedex.jsx
+++ src/Pokedex/Pokedex.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import Modal from '../Modal/Modal';
+import React, { useState } from "react";
+import axios from "axios";
+import Modal from "../Modal/Modal";
 
-import './Pokedex.scss'
+import "./Pokedex.scss";
 
 function Pokedex() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [pokemonData, setPokemonData] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [pokemonData, setPokemonData] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const [showModal, setShowModal] = useState(false);
 
   const searchPokemon = async () => {
@@ -16,20 +16,19 @@ function Pokedex() {
 
     try {
       const response = await axios.get(url);
-      console.log(response)
+      console.log(response);
 
       if (response.data) {
         setPokemonData(response.data);
-        setShowModal(true)
-        setErrorMessage('');
+        setShowModal(true);
+        setErrorMessage("");
       }
-
     } catch (error) {
       if (error.response && error.response.status === 404) {
-        setErrorMessage('Pokémon não encontrado!');
+        setErrorMessage("Pokémon não encontrado!");
         setPokemonData(null);
       } else {
-        setErrorMessage('Ocorreu um erro ao buscar o Pokémon.');
+        setErrorMessage("Ocorreu um erro ao buscar o Pokémon.");
         setPokemonData(null);
         console.error(error);
       }
@@ -37,7 +36,7 @@ function Pokedex() {
   };
 
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       searchPokemon();
     }
   };
@@ -58,16 +57,16 @@ function Pokedex() {
           placeholder="Digite o nome do Pokémon"
           onKeyDown={handleKeyPress}
         />
-        <button data-fs-button onClick={searchPokemon} id='search'>Buscar</button>
+        <button data-fs-button onClick={searchPokemon} id="search">
+          Search
+        </button>
       </div>
 
-      {showModal && (
-        <Modal pokemonData={pokemonData} closeModal={closeModal} />
-      )}
+      {showModal && <Modal pokemonData={pokemonData} closeModal={closeModal} />}
 
-      {errorMessage && <p>{errorMessage}</p>}
+      {errorMessage && <p data-fs-error-message>{errorMessage}</p>}
     </div>
   );
 }
 
-export default Pokedex
+export default Pokedex;
